69f2ee6b817fac88285a1c23002b0e5014bcfb00
noemi86-cpu <noemipatermo@gmail.com>
2021-06-07 17:51:43 +0200

    js-burger

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 // programma dovrà consentire di calcolare il prezzo del panino 
 //selezionando o deselezionando gli ingredienti proposti.
+var createSum = document.getElementById('somma-button');
+var resultSumElement = document.getElementById('result-sum');
+
+
+createSum.addEventListener('click', function () {
+ var priceBurger = 50;
+ var resultSumInt = 0;
+ var checkNum = document.getElementsByClassName('somma-check');
+ for (var i = 0; i < checkNum.length; i++){
+     if (checkNum[i].checked){
+
+         resultSumInt += parseInt(checkNum[i].value)
+     }
+     if(discount.value.length === 11){
+        
+     }
+ }
+
+ resultSumElement.innerHTML = 'il risultato è ' + resultSumInt;
+})
+
