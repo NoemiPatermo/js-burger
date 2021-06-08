3474886db24f4980f2ee6b8759d0626040351d39
noemi86-cpu <noemipatermo@gmail.com>
2021-06-08 14:48:26 +0200

    js-burger

--- js/script.js
+++ js/script.js
@@ -15,23 +15,21 @@ var userCoupon = document.getElementById("discount");
 
 generaPrezzo.addEventListener('click', function () {
  
- var resultSumInt = 20;
+    var resultSumInt = 20;
 
- var check = document.getElementsByClassName('somma-check');
+    var check = document.getElementsByClassName('somma-check');
 
- for (var i = 0; i < check.length; i++){
-     if (check[i].checked){
+    for (var i = 0; i < check.length; i++){
+        if (check[i].checked){
 
-         resultSumInt += parseInt(check[i].value);
-     }
-     
+            resultSumInt += parseInt(check[i].value);
+        }
+        
     
-} if(userCoupon.value.length === 6){
-    document.getElementById('your-price').innerHTML = 'Il costo del tuo burger è:  ' +  " $ "  + parseInt(resultSumInt - 20 / 100).toFixed(2);
-}
- 
- else{
-    price.innerHTML = 'Il costo del tuo burger è:  ' +  " $ "  +  resultSumInt.toFixed(2);
- }
-})
+    } if(userCoupon.value.length === 6){
+        document.getElementById('your-price').innerHTML = 'Il costo del tuo burger è:  ' +  " $ "  + parseInt(resultSumInt - 20 / 100).toFixed(2);
+    } else{
+        price.innerHTML = 'Il costo del tuo burger è:  ' +  " $ "  +  resultSumInt.toFixed(2);
+    }
+    })
 
